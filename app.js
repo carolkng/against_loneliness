ab9928f7bfa7ac8be36d55b4588f76ad9ae352d7
Carol Ng <carol@carolkng.com>
2019-11-16 12:38:46 -0800

    body

--- app.js
+++ app.js
@@ -191,6 +191,7 @@ function callSendAPI(sender_psid, response) {
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!')
+      console.log(body)
     } else {
       console.error("Unable to send message:" + err);
     }
