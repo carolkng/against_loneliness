73c02e1807c727c433f1b06c0bf12cfff42ebe61
NguyenBrianL97 <NguyenBrianL97@gmail.com>
2019-11-16 08:13:36 -0800

    testing sequence of text

--- app.js
+++ app.js
@@ -108,7 +108,10 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `You entered code: "${received_message.text}". Now beginning story...`,
+      "text": `You entered code: "${received_message.text}". Now beginning story...`
+    }
+
+    response = {
       "text": `You're name is Brian and you are hard at work.`
     }
   } else if (received_message.attachments) {
