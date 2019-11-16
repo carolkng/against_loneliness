770cf6c2344c8d41c02abd9c7d143fdb3205b722
Carol Ng <carol@carolkng.com>
2019-11-16 10:53:59 -0800

    add quick reply func

---
+++ response.js
@@ -0,0 +1,18 @@
+module.exports = class Response {
+  static genQuickReply(text, quickReplies) {
+    let response = {
+      text: text,
+      quick_replies: []
+    };
+
+    for (let quickReply of quickReplies) {
+      response["quick_replies"].push({
+        content_type: "text",
+        title: quickReply["title"],
+        payload: quickReply["payload"]
+      });
+    }
+
+    return response;
+  }
+}
