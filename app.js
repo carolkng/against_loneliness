fd3ec1b6053c2606c78bacbfa6dc56175dd8ff58
NguyenBrianL97 <NguyenBrianL97@gmail.com>
2019-11-16 08:18:50 -0800

    testing sequence of text

--- app.js
+++ app.js
@@ -110,13 +110,10 @@ function handleMessage(sender_psid, received_message) {
     response = {
       "text": `You entered code: "${received_message.text}". Now beginning story...`
     }
+    
     callSendAPI(sender_psid, response);
 
-    response = {
-      "text": `You're name is Brian and you are hard at work.`
-    }
-    callSendAPI(sender_psid, response);
-    
+
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
@@ -145,11 +142,10 @@ function handleMessage(sender_psid, received_message) {
         }
       }
     }
-    callSendAPI(sender_psid, response);
   } 
   
-  // Send the response message //send message right after
-  //callSendAPI(sender_psid, response);    
+  // Send the response message
+  callSendAPI(sender_psid, response);    
 }
 
 function handlePostback(sender_psid, received_postback) {
